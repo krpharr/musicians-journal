68e08de94fdb2fdb5c3dd0c1136ac1f3e57824b8
rpharr <rpharr@richmond.edu>
2019-12-13 00:21:31 -0500

    added date to main-view

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,13 @@ function init() {
     $("#embed-picker").datepicker("setDate", str);
     $("#embed-picked").text(str);
 
-    var m = moment("2019-12-13T09:00:00-05:00");
+    // var m = moment("2019-12-13T09:00:00-05:00");
+    var m = moment().hour(9).minute(0);
+    console.log("********** init() *****************");
+    console.log(m);
+
+    $("#main-date-ID").text(m.month() + 1 + "/" + m.date() + "/" + m.year());
+
     // var m = moment();
     // var roundUp = m.minute() || m.second() || m.millisecond() ? m.add(1, 'hour').startOf('hour') : m.startOf('hour');
     // start = start.hour();
